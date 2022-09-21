28fe1cdbbd327af5208c52fe35b2fce5de614545
diego <dsruggeri@gmail.com>
2022-09-21 19:40:11 -0300

    agregué funciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-//defino los dos equipo; y los inicializo en 0
+//Defino los equipos y pido por prompt los nombres.
+let equipoLocal = prompt("Ingrese el nombre del equipo local...");
+let equipoVisitante = prompt("Ahora, ingrese el nombre del equipo visitante.");
+
+//defino el contador de los equipos; y los inicializo en 0
 let local = 0;
 let visitante = 0;
 
@@ -13,56 +17,58 @@ let ronda = 0;
 // para eso, voy a usar 'tiro = Math.ceil(Math.random() * 2)'
 let tiro;
 
+//defino la funcion patear (local y visitante):
+function pateaLocal() {
+  document.write('<div class="row row-cols-12 row-cols-md-4">');
+  tiro = Math.ceil(Math.random() * 2);
+  if (tiro === 1) {
+    local += 1;
+    document.write(
+      '<div class="col"> <div class="card"> <img src="img/gol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea '
+    );
+    document.write(
+      equipoLocal + '...</h5><p class="card-text">Gol!</p></div></div></div>'
+    );
+  } else {
+    document.write(
+      '<div class="col"> <div class="card"> <img src="img/nogol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea '
+    );
+    document.write(
+      equipoLocal + '...</h5><p class="card-text">Afuera!</p></div></div></div>'
+    );
+  }
+}
+
+function pateaVisitante() {
+  tiro = Math.ceil(Math.random() * 2);
+  if (tiro === 1) {
+    visitante += 1;
+    document.write(
+      '<div class="col"> <div class="card"> <img src="img/gol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea '
+    );
+    document.write(
+      equipoVisitante +
+        '...</h5><p class="card-text">Gol!</p></div></div></div>'
+    );
+  } else {
+    document.write(
+      '<div class="col"> <div class="card"> <img src="img/nogol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea '
+    );
+    document.write(
+      equipoVisitante +
+        '...</h5><p class="card-text">Afuera!</p></div></div></div>'
+    );
+  }
+}
+
 //Cómo compruebo si hay ganador?
 // condiciones:
 // ronda tiene que ser 3 o más; no se puede ganar en menos de 3 rondas
 // si ronda es 3, y diferencia es 3; hay ganador.
 // si ronda es 4 y diferencia es 2, hay ganador
-//si ronda es 5 y diferencia es 1, hay ganador (OJO! VER CUÁNDO CAMBIA LA RONDA Y CUANDO COMPRUEBO)
+// si ronda es 5 y diferencia es 1, hay ganador (OJO! VER CUÁNDO CAMBIA LA RONDA Y CUANDO COMPRUEBO)
 // si ronda es >5, y diferencia es 1, hay ganador.
-
-//EMPIEZA LA DEFINICIÓN:
-while (ganador == false) {
-    document.write('<div class="row row-cols-12 row-cols-md-4">')
-  //tira local:
-  tiro = Math.ceil(Math.random() * 2);
-  console.log("Patea el equipo local...");
-  if (tiro === 1) {
-    local += 1;
-    console.log("Gol!!!")
-    document.write('<div class="col"> <div class="card"> <img src="img/gol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea el equipo Local...</h5><p class="card-text">Gol!</p></div></div></div>')
-  } else {
-    console.log("Afuera!")
-    document.write('<div class="col"> <div class="card"> <img src="img/nogol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea el equipo Local...</h5><p class="card-text">Afuera!</p></div></div></div>')
-  }
-
-  //tira visitante:
-  tiro = Math.ceil(Math.random() * 2);
-  console.log("Patea el equipo visitante...");
-  if (tiro === 1) {
-    visitante += 1;
-    console.log("Gol!!!");
-    document.write('<div class="col"> <div class="card"> <img src="img/gol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea el equipo Visitante...</h5><p class="card-text">Gol!</p></div></div></div>');
-  } else {
-    console.log("Afuera!");
-    document.write('<div class="col"> <div class="card"> <img src="img/nogol.png"  alt="..."> <div class="card-body"><h5 class="card-title">Patea el equipo Visitante...</h5><p class="card-text">Afuera!</p></div></div></div>');
-  }
-
-  //actualizo el contador de la tanda
-  ronda += 1;
-
-  //muestro el resultado parcial
-  console.log("Tanda número " + ronda);
-  console.log(
-    "Resultado Parcial: Local  " + local + "-" + visitante + " Visitante"
-  );
-
-
-  console.log("----------------------------------------------")
-
-  //compruebo si hay ganador.
-  //Al menos, se van a jugar 3 tandas, no se puede ganar antes de la 3ra tanda.
-
+function comprobarGanador() {
   if (ronda >= 3) {
     if (ronda === 3 && (local - visitante >= 3 || visitante - local >= 3)) {
       ganador = true;
@@ -78,15 +84,47 @@ while (ganador == false) {
       ganador = true;
     }
   }
- document.write('</div>')
- document.write('<h5> Restulado Parcial. Tanda nro ' + ronda + ': Local:  ' + local + '  -  ' + visitante + ' Visitante</h5>');
- document.write('<br>');
 }
 
-if(local > visitante ){
-    console.log("Ganó el equipo local.")
-    document.write('<h1> Ganó el equipo local </h1>');
-} else {
-    console.log("Ganó el equipo visitante.");
-    document.write('<h1> Ganó el equipo visitante </h1>');
+function anunciarGanador() {
+  if (local > visitante) {
+    document.write("<h1> Ganó " + equipoLocal + "!!!</h1>");
+  } else {
+    document.write("<h1> Ganó " + equipoVisitante + "!!!</h1>");
+  }
 }
+
+//EMPIEZA LA DEFINICIÓN:
+while (ganador == false) {
+  //patea local
+  pateaLocal();
+
+  //patea visitante:
+  pateaVisitante();
+
+  //actualizo el contador de la tanda
+  ronda += 1;
+
+  //compruebo si hay ganador.
+  //Al menos, se van a jugar 3 tandas, no se puede ganar antes de la 3ra tanda.
+
+  comprobarGanador();
+
+  document.write("</div>");
+  document.write(
+    "<h5> Restulado Parcial. Tanda nro " +
+      ronda +
+      ": " +
+      equipoLocal +
+      " " +
+      local +
+      "  -  " +
+      visitante +
+      " " +
+      equipoVisitante +
+      "</h5>"
+  );
+  document.write("<br>");
+}
+
+anunciarGanador();
